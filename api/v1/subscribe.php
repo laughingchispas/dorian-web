615ed55b3e2e9285f0f8ee0572519d8321e9e448
dudewad <me@laytonmiller.com>
2017-11-12 14:48:35 -0800

    Update subscriber api endpoint

--- api/v1/subscribe.php
+++ api/v1/subscribe.php
@@ -1,13 +1,10 @@
 <?php
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 if (isset($_POST['email'])) {
-    $headers = 'From: webmaster@doriandumont.com';
-
     $sent = mail(
-        'laughingchispas@gmail.com', //Change to Dorian's email address where he wants to receive the messages
+        'me@laytonmiller.com', //Change to Dorian's email address where he wants to receive the messages
         'New Email signup from DorianDumont.com', //Subject of the emails going to him
         "New email signup request: $email", //Body of the emails. Be sure to include $email
-        $headers
     );
 
     if($sent) {
@@ -17,6 +14,5 @@ if (isset($_POST['email'])) {
     echo "{message: \"Email request not sent for $email -- there was an error!\"}";
     die();
 }
-
 
 echo "{message: \"Email did not sent - no email was specified\"}";
