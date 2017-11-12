bdce289d2a0b92ece483a2634ae162fe5a115255
Nicole Miller <laughingchispas@gmail.com>
2017-11-12 23:38:56 +0100

    changes

--- api/v1/subscribe.php
+++ api/v1/subscribe.php
@@ -2,10 +2,11 @@
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 if (isset($_POST['email'])) {
     mail(
-        'laughingchispas@gmail.com', //Change to Dorian's email address where he wants to receive the messages
+        'me@laytonmiller.com', //Change to Dorian's email address where he wants to receive the messages
         'New Email signup from DorianDumont.com', //Subject of the emails going to him
         "New email signup request: $email" //Body of the emails. Be sure to include $email
     );
+    echo "mail sent";
 }
 
 echo "{message: \"Email request sent for $email\"}";
