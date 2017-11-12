19241ea2e0e1b57a6bbc3d7622cc53972572f542
dudewad <me@laytonmiller.com>
2017-11-12 14:33:59 -0800

    Update mail api to provide feedback for mail send results

--- api/v1/subscribe.php
+++ api/v1/subscribe.php
@@ -1,11 +1,22 @@
 <?php
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 if (isset($_POST['email'])) {
-    mail(
+    $headers = 'From: webmaster@doriandumont.com';
+
+    $sent = mail(
         'laughingchispas@gmail.com', //Change to Dorian's email address where he wants to receive the messages
         'New Email signup from DorianDumont.com', //Subject of the emails going to him
-        "New email signup request: $email" //Body of the emails. Be sure to include $email
+        "New email signup request: $email", //Body of the emails. Be sure to include $email
+        $headers
     );
+
+    if($sent) {
+        echo "{message: \"Email request sent for $email\"}";
+        die();
+    }
+    echo "{message: \"Email request not sent for $email -- there was an error!\"}";
+    die();
 }
 
-echo "{message: \"Email request sent for $email\"}";
+
+echo "{message: \"Email did not sent - no email was specified\"}";
